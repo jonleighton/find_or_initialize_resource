84d89fe0e1937e56917f939c7829308a29ff799b
Jon Leighton <j@jonathanleighton.com>
2007-09-27 10:59:35 +0000

    Allow the resource to be specified as the first foir parameter

--- lib/find_or_initialize_resource/controller_extensions.rb
+++ lib/find_or_initialize_resource/controller_extensions.rb
@@ -1,15 +1,30 @@
 module FindOrInitializeResource
   module ControllerExtensions
-    def find_or_initialize_resource(options = {})
+    def find_or_initialize_resource(*args)
+      options = args.last.is_a?(Hash) ? args.last : {}
+      options[:resource] = args.first unless args.first.is_a?(Hash)
+      
       FindOrInitializeResource::Builder.new(self, options).build
     end
     
-    def find_resource(options = {})
-      find_or_initialize_resource(options.merge(:features => [ :find ]))
+    def find_resource(*args)
+      find_or_initialize_resource(*process_args_for_foir(args, :find))
     end
     
-    def initialize_resource(options = {})
-      find_or_initialize_resource(options.merge(:features => [ :initialize ]))
+    def initialize_resource(*args)
+      find_or_initialize_resource(*process_args_for_foir(args, :initialize))
     end
+    
+    private
+    
+      def process_args_for_foir(args, *features)
+        if args.empty?
+          args = [{}]
+        elsif !args.first.is_a?(Hash) && args[1].nil?
+          args[1] = {}
+        end
+        args.last.merge!(:features => features)
+        args
+      end
   end
 end
--- spec/controller_extensions_spec.rb
+++ spec/controller_extensions_spec.rb
@@ -6,28 +6,37 @@ describe FindOrInitializeResource::ControllerExtensions do
     @controller_klass = Class.new(ActionController::Base)
   end
   
-  it "should create a new Builder, adding :features => [ :find, :initialize ] to the opts, " +
-     "when find_or_initialize_resource is called" do
-    
+  it "should create a new Builder, passing the opts, when find_or_initialize_resource is called" do
     FindOrInitializeResource::Builder.expects(:new).
-      with(@controller_klass, { :foo => :bar }).
+      with(@controller_klass, :foo => :bar).
       returns(stub_everything)
     
     @controller_klass.find_or_initialize_resource :foo => :bar
   end
   
-  it "should create a new Builder, adding :features => [ :find ], when find_resource is called" do
+  it "should create a new Builder, adding :features => [ :find ] to the opts, when find_resource is called" do
     FindOrInitializeResource::Builder.expects(:new).
-      with(@controller_klass, { :foo => :bar, :features => [ :find ] }).
+      with(@controller_klass, :foo => :bar, :features => [ :find ]).
       returns(stub_everything)
+    
     @controller_klass.find_resource :foo => :bar
   end
   
-  it "should create a new Builder, adding :features => [ :initialize ], when initialize_resource is called" do
+  it "should create a new Builder, adding :features => [ :initialize ] to the opts, when initialize_resource is called" do
     FindOrInitializeResource::Builder.expects(:new).
-      with(@controller_klass, { :foo => :bar, :features => [ :initialize ] }).
+      with(@controller_klass, :foo => :bar, :features => [ :initialize ]).
       returns(stub_everything)
+    
     @controller_klass.initialize_resource :foo => :bar
+  end
+  
+  it "should create a new Builder, adding :resource => :hammer to the opts, " +
+      "when find_or_initialize_resource is called with :hammer as the first parameter" do
+    FindOrInitializeResource::Builder.expects(:new).
+      with(@controller_klass, :resource => :hammer).
+      returns(stub_everything)
+    
+    @controller_klass.find_or_initialize_resource :hammer
   end
   
 end
